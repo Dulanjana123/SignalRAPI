414cd6217d80eb32de9d2a0fef66f9e4cc5590f9
Dulanjana <Dulanjana@DESKTOP-4MSIP85>
2023-04-21 23:01:18 +0530

    Add project files.

---
+++ SignalRAPI/Hub/MessageHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRAPI.Hub
+{
+    public class MessageHub : Hub<IMessageHubClient>
+    {
+        public async Task SendOffersToUser(List<string> message)
+        {
+            await Clients.All.SendOffersToUser(message);
+        }
+    }
+}
